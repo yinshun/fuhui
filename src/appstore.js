07aa3fcaec4f51c387caf8ff420210c305f38a9e
yap cheah shen <yapcheahshen@gmail.com>
2023-11-28 20:21:30 +0800

    2 books

---
+++ src/appstore.js
@@ -0,0 +1,18 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::fuhui',
+_ACC23.AppVer='23.11.28';
+_ACC23.AppName='fuhui'
+_ACC23.AppTitle='印順導師選集'
+_ACC23.allptks=['fuhui','fuhui-en'];
+_ACC23.selectedptks=['fuhui','fuhui-en']
+_ACC23.activeptk='fuhui'
+_ACC23.ptkinfo={
+    'fuhui':"中文",
+    'fuhui-en':"英文"
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
